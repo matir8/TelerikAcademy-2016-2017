aefc23cc95f48cd1030da8ae10961ade1aa2f032
matir8 <martin.markov28@gmail.com>
2016-04-12 10:06:30 +0300

    added first c# homework

---
+++ Homework/C-Sharp-Fundamentals/01. Introduction to Programming/09.Print Sequence/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Print_Sequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 2; i < 12; i++)
+            {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine("-{0}", i);
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
---
+++ Homework/C-Sharp-Fundamentals/01. Introduction to Programming/15.Age/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Age
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string date = Console.ReadLine();
+            char delimiter = '.';
+            String[] subStrings = date.Split(delimiter);
+            int month = int.Parse(subStrings[0]);
+            int day = int.Parse(subStrings[1]);
+            int year = int.Parse(subStrings[2]);
+
+            DateTime birthday = new DateTime(year, month, day);
+            DateTime today = DateTime.Now;
+
+            Console.WriteLine(Math.Truncate(((today - birthday).TotalDays)/365.0));
+            Console.WriteLine(10 + (Math.Truncate(((today - birthday).TotalDays) / 365.0)));
+        }
+    }
+}
