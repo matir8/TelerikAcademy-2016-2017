d20c1c2c26ee605e4ac2eefc48f051fb700f46b9
matir8 <martin.markov28@gmail.com>
2016-04-12 10:07:44 +0300

    Added files via upload

---
+++ Homework/C-Sharp-Fundamentals/02. Data types and variables/14. Print the ASCII Table/14. Print the ASCII Table/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14.Print_the_ASCII_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            for (int i = 33; i <= 126; i++)
+            {
+                char a = (char) i;
+                Console.Write(a);
+            }
+        }
+    }
+}
