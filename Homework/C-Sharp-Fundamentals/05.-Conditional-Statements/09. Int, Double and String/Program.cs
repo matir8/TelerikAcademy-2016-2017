26d16b60da8a96740049f5524d7b92757dea2f64
Martin Markov <martin.markov28@gmail.com>
2016-05-09 22:58:31 +0300

    Add files via upload

---
+++ Homework/C-Sharp-Fundamentals/05.-Conditional-Statements/09. Int, Double and String/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.Int__Double_and_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string type = Console.ReadLine();
+            if (type == "integer")
+            {
+                int i = int.Parse(Console.ReadLine());
+                i++;
+                Console.WriteLine(i);
+            }
+            else if (type == "real")
+            {
+                double n = double.Parse(Console.ReadLine());
+                n++;
+                Console.WriteLine("{0:F2}", n);
+            }
+            else if(type == "text")
+            {
+                string text = Console.ReadLine();
+                Console.WriteLine(text + "*");
+                ;
+            }
+        }
+    }
+}
