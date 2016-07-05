3fc859c5190df2d0e783e6127aefdd97c97e2084
matir8 <martin.markov28@gmail.com>
2016-07-05 14:11:09 +0300

    added js-homework

---
+++ Homework/C# Advanced/01. Arrays/01. Allocate array/01. Allocate array/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01.Allocate_array
+{
+    class AllocateArray
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = i * 5;
+                Console.WriteLine(arr[i]);
+            }
+            
+        }
+    }
+}
