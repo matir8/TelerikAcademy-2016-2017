3db6ddabf6b627ede339c1e5af74794fa60a2903
matir8 <martin.markov28@gmail.com>
2016-07-25 22:11:39 +0300

    solution for the first task of the HTML Template homework

---
+++ Homework/JavaScript-UI-and-DOM/11. HTML Templates/task-1.js
@@ -0,0 +1,27 @@
+function solve() {
+    return function (selector) {
+        var table = '<table class="items-table">' +
+            '<thead>' +
+            '<tr>' +
+            '<th>#</th>' +
+            '{{#headers}}' +
+            '<th>{{this}}</th>' +
+            '{{/headers}}' +
+            '</tr>' +
+            '</thead>' +
+            '<tbody>' +
+            '{{#items}}' +
+            '<tr>' +
+            '<td>{{@index}}</td>' +
+            '<td>{{this.col1}}</td>' +
+            '<td>{{this.col2}}</td>' +
+            '<td>{{this.col3}}</td>' +
+            '</tr>' +
+            '{{/items}}' +
+            '</tbody>' +
+            '</table>';
+
+
+        $(selector).html(table);
+    };
+}
